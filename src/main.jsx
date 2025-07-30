1d12b3b789febcfe56b7a876e42e3b4bb8e23cd8
刘博 <583139315@qq.com>
2025-07-30 10:58:38 +0800

    feat: 日常提交

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./router/index.jsx";
 import { BrowserRouter } from "react-router-dom";
-// import { worker } from "@/mocks/browser.js";
-// if (import.meta.env.MODE === "development") {
-//   // 启动时添加错误处理
-//     worker.start({onUnhandledRequest: 'bypass'});
-// }
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
